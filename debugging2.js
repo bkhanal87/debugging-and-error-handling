aeb2826be390005273d67725f5ba26f7f65bff24
bkhanal87@gmail.com <bkhanal87@gmail.com>
2022-09-24 18:11:43 -0400

    debugging techniques

---
+++ debugging2.js
@@ -0,0 +1,16 @@
+// Debugging JS: https://developer.chrome.com/docs/devtools/javascript/
+
+// The Sources panel is where you debug JavaScript.
+
+// The Sources panel has three parts:
+
+// The File Navigator pane. Every file that the page requests is listed here.
+// The Code Editor pane. After selecting a file in the File Navigator pane, the contents of that file are displayed here.
+// The JavaScript Debugging pane. Various tools for inspecting the page's JavaScript. If your DevTools window is wide, this pane is displayed to the right of the Code Editor pane.
+
+
+// // Errors VS. Warnings
+// Many people are met with warnings and treat them as errors. Errors will stop the execution of your program or whatever process you may be attempting to run and prevent further action. Warnings, on the other hand, are messages that provide you insight on potential problems that may not necessarily crash your program at runtime, or at all! While you should address these warnings if possible and as soon as possible, warnings are not as significant as errors and are more likely to be informational. Warnings are typically shown in yellow, while errors are typically shown in red. Though these colors are not a rule, frequently there will be a visual differentiation between the two, regardless of the platform you are encountering them on.
+
+// other Console methods: console.table(), console.trace(), visit: https://www.w3schools.com/jsref/obj_console.asp
+
